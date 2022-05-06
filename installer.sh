c69dee2823124b2db0feb4cc6fc76034a0e191ce
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-06 23:38:28 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,61 +4,12 @@
 ##
 ###########################################
 ###########################################
+#!/bin/sh
+echo
 
-# my config script #
-
-MY_IPK_PY2="https://github.com/tar1971/feeds-finder"
-MY_IPK_PY3="https://github.com/tar1971/feeds-finder"
-MY_URL="https://github.com/tar1971/feeds-finder/blob/main/feeds-finder_V1.6_py2_all.ipk?raw=true"
-MY_URL="https://github.com/tar1971/feeds-finder/blob/main/feeds-finder_V1.6_py3_all.ipk?raw=true"
-PYTHON_VERSION=$(python -c 'import sys; print(sys.version_info[0])')
-
-######################################################################################
-MY_EM='============================================================================================================'
-#  Remove Old Plugin  #
-echo "   >>>>   Remove old version   "
-
-rm -r /usr/lib/enigma2/python/Plugins/Extensions/FeedsFinder
-
-#################################
-    
-###################
-echo "============================================================================================================================"
- echo " DOWNLOAD AND INSTALL PLUGIN "
-
-echo "   Install Plugin please wait "
-
-cd /tmp 
-
-set -e
-    
-  wait
-     
-if python --version 2>&1 | grep -q '^Python 3\.'; then
-  wget "$MY_URL/$MY_IPK_PY3"
-opkg install --force-overwrite $MY_IPK_PY3
-	else 
-echo "   Install Plugin please wait "
-   wget "$MY_URL/$MY_IPK_PY2"
-opkg install --force-overwrite $MY_IPK_PY2
-	fi
-echo "================================="
-set +e
-cd ..
-wait
-rm -f /tmp/$MY_IPK_PY2
-rm -f /tmp/$MY_IPK_PY3
-	if [ $? -eq 0 ]; then
-echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
-fi
-		echo "********************************************************************************"
-echo "   UPLOADED BY  >>>>  TAR_TAR " 
-sleep 4;                         
-echo $MY_EM
-###################                                                                                                                  
-echo " Your Device Will RESTART Now " 
-echo "**********************************************************************************"
-wait
-init 4
-init 3
+opkg install --force-overwrite  https://github.com/tar1971/feeds-finder/blob/main/feeds-finder_V1.6_py2_all.ipk?raw=true
+sleep 2;
 exit 0
+
+
+
